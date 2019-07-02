2f2e8b8082b67712c47cb4aef763979106638723
Arif Hossain <arif.hossain@thedailystar.net>
2019-07-02 16:30:25 +0600

    relationship assigned to models

--- app/Question.php
+++ app/Question.php
@@ -4,7 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Question extends Model
-{
-    //
+class Question extends Model {
+
+	public function user() {
+		return $this->belongsTo( 'App\User' );
+	}
+
+	public function category() {
+		return $this->belongsTo( 'App\Category' );
+	}
+
+	public function replies() {
+		return $this->hasMany( 'App\Reply' );
+	}
 }
